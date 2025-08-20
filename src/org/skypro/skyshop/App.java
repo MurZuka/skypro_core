2723ef1f56b073c109dedc697d95a3759031435f
MurZuka <murzuka@yahoo.com>
2025-08-20 22:01:09 +0300

    Домашнее задание по уроку «ООП. Полиморфизм. Интерфейсы»

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -19,39 +19,6 @@ public class App {
         FixPriceProduct p4 = new FixPriceProduct("Фиксированный чайник");
         DiscountedProduct p5 = new DiscountedProduct("Скидочный тостер", 350, (byte) 5);
 
-        //ProductBasket basket = new ProductBasket(new Product[]{p1, p2, p3, p4, p5});
-
-        //Добавление продукта в корзину.
-        //basket.addProduct(p3);
-        //basket.addProduct(p3);
-
-        //Добавление продукта в заполненную корзину, в которой нет свободного места.
-        //basket.addProduct(p3);
-
-        //Печать содержимого корзины с несколькими товарами.
-        //System.out.println(basket);
-
-        //Получение стоимости корзины с несколькими товарами.
-        //System.out.println(basket.getTotalCost());
-
-        //Поиск товара, который есть в корзине.
-        //System.out.println(basket.isProductInBasket("Микроволновка"));
-
-        //Поиск товара, которого нет в корзине.
-        //System.out.println(basket.isProductInBasket("Вундервафля"));
-
-        //Очистка корзины.
-        //basket.purgeBasket();
-
-        //Печать содержимого пустой корзины.
-        //System.out.println(basket);
-
-        //Получение стоимости пустой корзины.
-        //System.out.println(basket.getTotalCost());
-
-        //Поиск товара по имени в пустой корзине.
-        //System.out.println(basket.isProductInBasket("Микроволновка"));
-
         Article a1 = new Article("Статья 1", "Текст статьи раз");
         Article a2 = new Article("Описание 2", "Текст описания два");
 
